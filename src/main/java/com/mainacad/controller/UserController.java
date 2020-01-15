5f33c06d48c294d6409d20545779b599671fd3b9
Alex <ignatenko2207@gmail.com>
2020-01-15 20:54:59 +0200

    Change auth controller

--- src/main/java/com/mainacad/controller/UserController.java
+++ src/main/java/com/mainacad/controller/UserController.java
@@ -4,9 +4,12 @@ import com.mainacad.model.User;
 import com.mainacad.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("user")
@@ -34,10 +37,10 @@ public class UserController {
         return new ResponseEntity(user, HttpStatus.OK);
     }
 
-    @GetMapping("auth")
-    public ResponseEntity getByLoginAndPassword(@RequestParam String login,
-                                                @RequestParam String password) {
-        User user = userService.getByLoginAndPassword(login, password);
+    @PostMapping("auth")
+    public ResponseEntity getByLoginAndPassword(@RequestBody String body) {
+        Map<String, Object> map = new JacksonJsonParser().parseMap(body);
+        User user = userService.getByLoginAndPassword((String) map.get("login"), (String) map.get("password"));
         if (user == null) {
             return new ResponseEntity(HttpStatus.FORBIDDEN);
         }
--- src/main/java/com/mainacad/model/User.java
+++ src/main/java/com/mainacad/model/User.java
@@ -1,5 +1,6 @@
 package com.mainacad.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
