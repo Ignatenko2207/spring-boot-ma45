2d17487bbf4b306d5302e5ddf0e17148a7c93473
Alex <ignatenko2207@gmail.com>
2020-01-22 21:24:47 +0200

    Add cron

--- src/main/java/com/mainacad/scheduler/CurrencyExtractionService.java
+++ src/main/java/com/mainacad/scheduler/CurrencyExtractionService.java
@@ -1,5 +1,7 @@
 package com.mainacad.scheduler;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -7,26 +9,39 @@ import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @Component
 public class CurrencyExtractionService {
 
     private List<String> list = Collections.synchronizedList(new ArrayList<>());
+    private AtomicLong extractionTime = new AtomicLong(new Date().getTime());
 
-    @Scheduled(initialDelay = 1000, fixedDelay = 5000)
+//    @Scheduled(cron = "0-59/2 * * * * *")
     public void getUkrNetResponse() {
-         list.add("https://www.ukr.net/ - " + Instant.now());
+         list.add(String.valueOf(new Date().getTime()));
     }
 
-    @Scheduled(initialDelay = 1000, fixedDelay = 2000)
+//    @Scheduled(initialDelay = 1000, fixedDelay = 20000)
     public void writeData() {
         if (list.isEmpty()) {
             return;
         }
-        list.forEach(text -> log.info(text));
 
-        list.clear();
+        List<String> elementsToRemove  = new ArrayList<>();
+        for (String text : list) {
+            if (Long.parseLong(text) >= extractionTime.get()) {
+                log.info(new Date(Long.parseLong(text)).toString());
+            }
+            else {
+                elementsToRemove.add(text);
+            }
+        }
+
+        list.removeAll(elementsToRemove);
+        extractionTime.set(new Date().getTime());
     }
 
 }
